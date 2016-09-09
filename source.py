6a4183dcd875a10ae3e9ed8454d4b4d37e1cb28c
JuliaHeleneW <jwerner@csumb.edu>
2016-09-09 06:21:31 -0700

    Update source.py

--- source.py
+++ source.py
@@ -8,37 +8,32 @@ image6=Image.open("6.png")
 image7=Image.open("7.png")
 image8=Image.open("8.png")
 image9=Image.open("9.png")
+image10=Image.new("RGB",(495,557),"white")
 images=[image1,image2,image3,image4,image5,image6,image7,image8,image9]
-print images
 redPixelList=[]
 greenPixelList=[]
 bluePixelList=[]
-myList=[]
-for image in images:
-    print(image.size)
-    for x in range(0,pictureWidth):
-        for y in range(0,pictureHeight):
-	         for myImage in images:
-                 myRed,myGreen,myBlue=myImage.getPixel((x,y))
-			         return myRed,myGreen,myBlue
-					 
-redPixelList.append(myRed)
-greenPixelList.append(myGreen)
-bluePixelList.append(myBlue)
-def medianOdd(myList):
-    listLength=len(myList)
-    sortedValues=sorted(myList)
-    middleIndex=((listLength+1)/2)-1
-return sortedValues[middleIndex]
-medianRed=medianOdd(redPixelList)
-medianGreen=medianOdd(greenPixelList)
-medianBlue=medianOdd(bluePixelList)
-redPixelList.remove(myRed)
-greenPixelList.remove(myGreen)
-bluePixelList.remove(myBlue)
-for x in range(0,pictureWidth):
-    for y in range(0,pictureWidth)
-	     outputImage=new.image(x,y,medianRed,medianGreen,medianBlue)
-		 print outputImage
-
-		
+for Image in images:
+    width,height=Image.size
+    for x in range(0,width):
+        for y in range(0,height):
+	         for Image in images:
+                  red, green, blue = Image.getpixel((x,y))
+                  redPixelList.append(red)
+                  greenPixelList.append(green)
+                  bluePixelList.append(blue)
+                  redPixelList.sort()
+                  greenPixelList.sort()
+                  bluePixelList.sort()
+                  from statistics import median
+                  medianRed=median(redPixelList)
+                  medianGreen=median(greenPixelList)
+                  medianBlue=median(bluePixelList)
+                  redPixelList.remove(red)
+                  greenPixelList.remove(green)
+                  bluePixelList.remove(blue)
+                  pix=image10.load()
+                  for x in range(0,width):
+                      for y in range(0,height):
+                          pix[x,y]=medianRed,medianGreen,medianBlue
+                          image10=Image.save("D://Image/outputImage.png")
